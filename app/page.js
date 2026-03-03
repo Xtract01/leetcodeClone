a2640db119bcf96d5f2ac6389b812eb4e47f5a0c
Xtract01 <meena.balwir149@gmail.com>
2026-03-04 01:47:26 +0530

    feat: integrate Clerk for authentication with sign-in and sign-up pages

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <Button>click Me</Button>
+      <UserButton />
     </div>
   );
 }
